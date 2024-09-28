bf2a6e6d6490c7736a8029a541f291a1dd3ab9ef
YugBandhara <ybandhara@gmail.com>
2024-09-28 17:08:12 -0400

    added smple componenet

---
+++ src/Sample.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Define the component as a functional component
+const Sample: React.FC = () => {
+  return (
+    <div>
+      <p>First component</p>
+    </div>
+  );
+};
+
+export default Sample;
